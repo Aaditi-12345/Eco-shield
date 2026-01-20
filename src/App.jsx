783b17fcb362859cda8a39f2326b4e45fe96373d
Aaditi-12345 <bama2958@gmail.com>
2026-01-20 21:22:10 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -256,8 +256,14 @@ export default function App() {
               </div>
             </div>
             <div className="h-[500px] rounded-[3rem] overflow-hidden shadow-3xl border-[12px] border-slate-50">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m12!1m18!1m12!1m3!1d112833.62319047128!2d153.3323055!3d-28.0166591!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b911a367468536b%3A0xc3f7a26f0376786a!2sGold%20Coast%20QLD!5e0!3m2!1sen!2sau!4v1700000000000!5m2!1sen!2sau" width="100%" height="100%" style={{ border: 0 }} loading="lazy"></iframe>
-            </div>
+              <iframe 
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112834.11550186196!2d153.30133481232822!3d-28.021436423021952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b910a4ced58239f%3A0x502a35af3de8d60!2sGold%20Coast%20QLD!5e0!3m2!1sen!2sau!4v1700000000000!5m2!1sen!2sau" 
+  width="100%" 
+  height="100%" 
+  style={{ border: 0 }} 
+  allowFullScreen="" 
+  loading="lazy"
+></iframe>
           </div>
         </div>
       </section>
