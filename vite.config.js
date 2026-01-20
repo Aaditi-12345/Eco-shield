a8ee686eec25ec63649c476cac19f6d1b7257ce9
Aaditi-12345 <bama2958@gmail.com>
2026-01-20 22:06:03 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Add the DOT before the slash
+  base: '/', // Use a single slash for custom domains
 })
